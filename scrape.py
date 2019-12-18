09afbec88a97220949b721b78a8248ef01c5ab80
Jimmy Manninen <jimmmann100@student.kau.se>
2019-12-18 21:30:42 +0100

    Working, not done.

--- scrape.py
+++ scrape.py
@@ -1,7 +1,6 @@
 from requests_html import HTMLSession
-from bs4 import BeautifulSoup
-
-http://theautomatic.net/2019/01/19/scraping-data-from-javascript-webpage-python/
+from bs4 import BeautifulSoup as soup
+import pyppdf.patch_pyppeteer
 
 # Create an HTML Session object
 session = HTMLSession()
@@ -12,14 +11,29 @@ resp = session.get('https://www.tradera.com/search?q=lundby&itemStatus=Ended')
 # Run JavaScript code on webpage
 resp.html.render()
 
+# Html parsing
+page_soup = soup(resp.html.html, 'lxml')
 
+# Grabs each product
+containers = page_soup.findAll("div", {"class": "item-card-container"})
 
+# Save to csv file
+filename = "products.csv"
+f = open(filename, "w")
 
+headers = "title, auction_price, bids\n"
 
+f.write(headers)
 
+for container in containers:
+    title = container.div.p.a["title"]
 
+    sell_price_container = container.findAll("span", {"class": "text-nowrap font-weight-bold item-card-details-price"})
+    auction_price = sell_price_container[0].text
 
+    bid_container = container.findAll("span", {"class": "text-nowrap mr-2"})
+    bids = bid_container[0].text
 
-
-
-
+    print("Title: " + title)
+    print("Auction_price " + auction_price)
+    print("Bids: " + bids)
